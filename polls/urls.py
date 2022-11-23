a42c265e49e1cf96918537691c6c95c1a3519353
alikpid <koromislo.molodec@gmail.com>
2022-11-23 11:44:58 +0700

    бут работает. отсановилась на форме регистрации

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-
+from .views import BBLoginView
+from .views import profile
+from .views import BBLogoutView
+from .views import ChangeUserInfoView
 from . import views
 
 
 app_name = 'polls'
 urlpatterns = [
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/login', BBLoginView.as_view(), name='login'),
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,48 @@ from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from .forms import ChangeUserInfoForm
+from .models import AdvUser
+
+
+class BBLoginView(LoginView):
+    template_name = 'polls/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'polls/profile.html')
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'polls/logout.html'
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin,
+                         UpdateView):
+    model = AdvUser
+    template_name = 'polls/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('polls:profile')
+    success_message = 'Личные данные пользователя изменены'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
 
 
 class IndexView(generic.ListView):
